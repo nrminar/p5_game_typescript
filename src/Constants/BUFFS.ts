2440165b5e68d856554a2314fb2f9b50b7b878ff
nrminar <nrminar@gmail.com>
2023-10-22 10:02:39 -0500

    Updates

--- src/Constants/BUFFS.ts
+++
@@ -1,9 +0,0 @@
-enum BUFFS {
-    ATTACK_SPEED = 'Attack Speed +1',
-    SHIP_SPEED = 'Ship Speed +1',
-    EXTRA_LIFE = 'More Life',
-    EXTRA_PROJECTILE = 'Projectile +1',
-    EXP_RANGE = 'EXP Pickup Range +1',
-}
-
-export default BUFFS
--- src/Constants/KEYCODE.ts
+++
@@ -1,9 +0,0 @@
-const enum KEYCODE {
-    //KEYCODE CONSTANTS
-    UP = 87,
-    RIGHT = 68,
-    LEFT = 65,
-    ESCAPE = 27,
-}
-
-export default KEYCODE
